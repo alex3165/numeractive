609d310a3f5a45067e7e30c150921e848513aecb
alex3165 <alexr.3165@gmail.com>
2014-04-30 18:39:37 +0200

    modifs api.js

--- node/app.js
+++ node/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 var api = require('./routes/api');
-/* access to database file */
-var db = require('./db');
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
@@ -18,189 +16,29 @@ app.get('/partials/:page', routes.partials);
 
 app.get('/admin', routes.admin);
 
+/************ API ******************/
+app.get('/api/login', api.login); // try login and password connection 
+
 /* posts */
-//app.get('/api/posts', api.posts);
-//app.get('/api/post/:id', api.post);
-app.post('/api/post', api.addPost);
-app.put('/api/post/:id', api.editPost);
-app.delete('/api/post/:id', api.deletePost);
+app.get('/api/posts', api.posts); // Get all posts
+app.get('/api/post/:id', api.post); // Get post from id
+app.post('/api/post', api.addPost); // Add post 
+// app.put('/api/post/:id', api.editPost);
+// app.delete('/api/post/:id', api.deletePost);
 
 /* categories */
-app.get('/api/categories', api.categories);
-app.get('/api/category/:id', api.category);
-app.post('/api/category', api.addCategory);
-app.put('/api/category/:id', api.editCategory);
-app.delete('/api/category/:id', api.deleteCategory);
+// app.get('/api/categories', api.categories);
+// app.get('/api/category/:id', api.category);
+// app.post('/api/category', api.addCategory);
+// app.put('/api/category/:id', api.editCategory);
+// app.delete('/api/category/:id', api.deleteCategory);
 
-app.get('/api/login',function(req, res){
-    var user = {
-        login : req.query.login,
-        mdp : req.query.mdp
-    };
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('SELECT * FROM users WHERE login="'+user.login+'"',function(err,rows){
-                if (!err) {
-                    console.log(rows);
-                    if (user.mdp == rows[0].mdp) {
-                        res.send({status : 'success'});
-                    }
-                }else{
-                    res.send(err);
-                }
-                db.end();
-            });
-        }else{
-            res.send(err);
-        }
-    });
-});
 
-/* get posts using database */
-app.get('/api/posts',function(req,res){
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('SELECT * FROM posts',function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    var posts = [];
-                    rows.forEach(function(post, i) {
-                        posts.push({
-                            id: i,
-                            title: post.title,
-                            text: post.text.substr(0, 300) + ' ...',
-                            category: post.category,
-                            img: post.img,
-                            creationDate: post.creation
-                        });
-                    });
-                    res.send(posts);
-                }
-                db.end();
-            });
-        }
-    });
-});
-app.get('/api/post/:id', function(req, res){
-    var id = req.param('id');
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('SELECT * FROM posts WHERE id="'+id+'"',function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    res.send({posts : rows});
-                }
-                db.release();
-            });
-        }else{
-            res.send(err);
-        }
-    });
+/* Users */
+app.get('/api/users', api.users); // Get all users
+app.get('/api/user/:login', api.user); // Get user with specific id
+/*********** API END ****************/
 
-});
 
-// app.post('/api/post', function(req, res){
 
-// });
-/* CRUD USER */
 
-app.get('/api/users', function(req, res){
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('SELECT * FROM users',function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    res.send({posts : rows});
-                }
-                db.end();
-            });
-        }else{
-            res.send(err);
-        }
-    });
-});
-
-app.get('/api/user/:login', function(req, res){
-    var login = req.param('login');
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('SELECT * FROM users WHERE login="'+login+'"',function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    res.send({posts : rows});
-                }
-                db.end();
-            });
-        }else{
-            res.send(err);
-        }
-    });
-});
-
-app.post('/api/user', function(req, res){
-    var user = {
-        name : req.body.name,
-        login : req.body.login,
-        mdp : req.body.mdp
-    };
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('INSERT INTO users SET ?', user,function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    res.send({posts : rows});
-                }
-                db.end();
-            });
-        }else{
-            res.send(err);
-        }
-    });
-});
-
-app.put('/api/user/:login', function(req, res){
-    var login = req.params.login;
-    var newuser = {
-        name : req.body.name,
-        login : req.body.login,
-        mdp : req.body.mdp
-    };
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('UPDATE users SET ? WHERE login="'+login+'"', newuser,function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    res.send({posts : rows});
-                }
-                db.end();
-            });
-        }else{
-            res.send(err);
-        }
-    });
-
-});
-
-app.delete('/api/user/:login', function(req, res){
-    var login = req.params.login;
-    db.getConnection(function(err,db){
-        if (!err) {
-            db.query('DELETE FROM users WHERE login="'+login+'"',function(err,rows){
-                if (err) {
-                    res.send(err);
-                }else{
-                    res.send({posts : rows});
-                }
-                db.end();
-            });
-        }else{
-            res.send(err);
-        }
-    });
-});
--- node/routes/api.js
+++ node/routes/api.js
@@ -1,106 +1,172 @@
-var data = {
-    "posts": [{
-        "title": "Qui frappuccino trifecta to go milk,",
-        "text": "Extraction skinny, sweet plunger pot wings turkish saucer single shot cappuccino, fair trade coffee as in, mazagran, caramelization cortado est acerbic cortado roast sit siphon. Siphon fair trade, est filter est siphon extraction instant crema, cup, shop, aged sugar to go filter variety at roast seasonal barista seasonal. Macchiato, half and half iced medium chicory single shot, whipped, galão, strong aromatic, a bar milk americano plunger pot. Carajillo half and half sweet et cappuccino est chicory, steamed aftertaste instant in spoon rich spoon galão frappuccino, organic, café au lait in shop dripper eu steamed. Blue mountain instant, as roast extraction arabica brewed cup, body iced, pumpkin spice to go id organic frappuccino caffeine percolator cup qui percolator cream.",
-        "category": 3,
-        "img": "images/img2.png",
-        "creationDate": "24 juillet 2013"
-    }, {
-        "title": "Skinny, coffee aromatic, acerbic crema eu acerbic single shot",
-        "text": "Aromatic, sit french press cinnamon, aftertaste java aroma and, dripper variety qui to go, at to go affogato macchiato extra carajillo saucer affogato con panna. Grounds, frappuccino cinnamon turkish espresso filter seasonal, so whipped, spoon to go that aromatic flavour. Spoon trifecta latte, flavour macchiato brewed strong so whipped spoon acerbic mocha rich. Milk, cream seasonal decaffeinated fair trade et robust to go grounds, extra galão, mocha latte cup, aged shop beans, cortado crema macchiato mocha steamed.",
-        "category": 0,
-        "img": "images/img1.jpg",
-        "creationDate": " 38 decembre 8999"
-    },{
-        "title": "Leave dead animals as gifts climb leg sun bathe",
-        "text": "Hate dog climb leg or chase imaginary bugs. Nap all day. Swat at dog sleep on keyboard yet leave dead animals as gifts so hunt anything that moves leave dead animals as gifts or intrigued by the shower chase imaginary bugs. Hate dog destroy couch and need to chase tail. Stick butt in face destroy couch for leave hair everywhere and shake treat bag chew iPad power cord for throwup on your pillow for missing until dinner time. Chase imaginary bugs.",
-        "category": 3,
-        "img": "images/img5.jpg",
-        "creationDate": " 78 aout 1111"
-    },{
-        "title": "Mark territory hide when guests come over flop over so stretch leave dead animals as gifts",
-        "text": "Rub face on everything intently stare at the same spot so claw drapes and mark territory but need to chase tail use lap as chair. Why must they do that swat at dog. Climb leg stick butt in face yet intently sniff hand but flop over for play time find something else more interesting. Chew iPad power cord climb leg under the bed chew foot leave dead animals as gifts chase imaginary bugs hopped up on goofballs. Inspect anything brought into the house climb leg for give attitude yet hunt anything that moves inspect anything brought into the house but stick butt in face.",
-        "category": 1,
-        "img": "images/img4.jpg",
-        "creationDate": " 20 septembre 2010"
-    }]
+var db = require('./db');
+
+// var data = {
+//     "posts": [{
+//         "title": "Qui frappuccino trifecta to go milk,",
+//         "text": "Extraction skinny, sweet plunger pot wings turkish saucer single shot cappuccino, fair trade coffee as in, mazagran, caramelization cortado est acerbic cortado roast sit siphon. Siphon fair trade, est filter est siphon extraction instant crema, cup, shop, aged sugar to go filter variety at roast seasonal barista seasonal. Macchiato, half and half iced medium chicory single shot, whipped, galão, strong aromatic, a bar milk americano plunger pot. Carajillo half and half sweet et cappuccino est chicory, steamed aftertaste instant in spoon rich spoon galão frappuccino, organic, café au lait in shop dripper eu steamed. Blue mountain instant, as roast extraction arabica brewed cup, body iced, pumpkin spice to go id organic frappuccino caffeine percolator cup qui percolator cream.",
+//         "category": 3,
+//         "img": "images/img2.png",
+//         "creationDate": "24 juillet 2013"
+//     }, {
+//         "title": "Skinny, coffee aromatic, acerbic crema eu acerbic single shot",
+//         "text": "Aromatic, sit french press cinnamon, aftertaste java aroma and, dripper variety qui to go, at to go affogato macchiato extra carajillo saucer affogato con panna. Grounds, frappuccino cinnamon turkish espresso filter seasonal, so whipped, spoon to go that aromatic flavour. Spoon trifecta latte, flavour macchiato brewed strong so whipped spoon acerbic mocha rich. Milk, cream seasonal decaffeinated fair trade et robust to go grounds, extra galão, mocha latte cup, aged shop beans, cortado crema macchiato mocha steamed.",
+//         "category": 0,
+//         "img": "images/img1.jpg",
+//         "creationDate": " 38 decembre 8999"
+//     },{
+//         "title": "Leave dead animals as gifts climb leg sun bathe",
+//         "text": "Hate dog climb leg or chase imaginary bugs. Nap all day. Swat at dog sleep on keyboard yet leave dead animals as gifts so hunt anything that moves leave dead animals as gifts or intrigued by the shower chase imaginary bugs. Hate dog destroy couch and need to chase tail. Stick butt in face destroy couch for leave hair everywhere and shake treat bag chew iPad power cord for throwup on your pillow for missing until dinner time. Chase imaginary bugs.",
+//         "category": 3,
+//         "img": "images/img5.jpg",
+//         "creationDate": " 78 aout 1111"
+//     },{
+//         "title": "Mark territory hide when guests come over flop over so stretch leave dead animals as gifts",
+//         "text": "Rub face on everything intently stare at the same spot so claw drapes and mark territory but need to chase tail use lap as chair. Why must they do that swat at dog. Climb leg stick butt in face yet intently sniff hand but flop over for play time find something else more interesting. Chew iPad power cord climb leg under the bed chew foot leave dead animals as gifts chase imaginary bugs hopped up on goofballs. Inspect anything brought into the house climb leg for give attitude yet hunt anything that moves inspect anything brought into the house but stick butt in face.",
+//         "category": 1,
+//         "img": "images/img4.jpg",
+//         "creationDate": " 20 septembre 2010"
+//     }]
+// };
+
+
+// var datacat = {
+//     "categories": [{
+//         "type": "programmation"
+//     }, {
+//         "type": "design"
+//     }, {
+//         "type": "creative coding"
+//     }, {
+//         "type": "vie quotidienne"
+//     }]
+// };
+
+
+
+/* Login for API */
+
+exports.login = function(req, res){
+    var user = {
+        login : req.query.login,
+        mdp : req.query.mdp
+    };
+    db.getConnection(function(err,db){
+        if (!err) {
+            db.query('SELECT * FROM users WHERE login=?',user.login ,function(err,rows){
+                if (!err) {
+                    console.log(rows);
+                    if (user.mdp == rows[0].mdp) {
+                        res.send({status : 'success'});
+                    }
+                }else{
+                    res.send(err);
+                }
+                db.end();
+            });
+        }else{
+            res.send(err);
+        }
+    });
 };
-
-
-var datacat = {
-    "categories": [{
-        "type": "programmation"
-    }, {
-        "type": "design"
-    }, {
-        "type": "creative coding"
-    }, {
-        "type": "vie quotidienne"
-    }]
-};
-
-
 
 
 /*  POST PART OF APPI  */
 
 exports.posts = function(req, res) {
-    var posts = [];
-    data.posts.forEach(function(post, i) {
-        posts.push({
-            id: i,
-            title: post.title,
-            text: post.text.substr(0, 300) + ' ...',
-            category: post.category,
-            img: post.img,
-            creationDate: post.creationDate
-        });
+    db.getConnection(function(err,db){
+        if (!err) {
+            db.query('SELECT * FROM posts',function(err,rows){
+                if (err) {
+                    res.send(err);
+                }else{
+                    var posts = [];
+                    rows.forEach(function(post, i) {
+                        posts.push({
+                            id: i,
+                            title: post.title,
+                            text: post.text.substr(0, 300) + ' ...',
+                            category: post.category,
+                            img: post.img,
+                            creationDate: post.creation
+                        });
+                    });
+                    res.send(posts);
+                }
+                db.end();
+            });
+        }
     });
-    res.send(posts);
 };
 
 exports.post = function(req, res) {
-    var id = req.param.id;
-    if (id >= 0 && id < data.posts.length)
-        res.send(data.posts[req.param.id]);
-    else
-        res.send(404, {
-            status: "error"
-        });
-};
-
-exports.addPost = function(req, res) {
-    data.posts.push(req.body);
-    res.send(404, {
-        status: "ok"
+    var id = req.param('id');
+    db.getConnection(function(err,db){
+        if (!err) {
+            db.query('SELECT * FROM posts WHERE id=?',id ,function(err,rows){
+                if (err) {
+                    res.send(err);
+                }else{
+                    res.send({posts : rows});
+                }
+                db.release();
+            });
+        }else{
+            res.send(err);
+        }
     });
 };
 
-exports.editPost = function(req, res) {
-    var id = req.params.id;
-    if (id >= 0 && id < data.posts.length) {
-        data.posts[id] = req.body;
-        res.send({
-            status: "ok"
-        });
-    } else
-        res.send(404, {
-            status: "error"
-        });
+exports.addPost = function(req, res) {
+    
+    var newpost = {
+        title : req.body.title,
+        text : req.body.text,
+        img : req.body.img,
+        id_cat : req.body.categorie,
+        id_user : req.body.userid
+    };
+
+    db.getConnection(function(err,db){
+        if (!err) {
+            db.query('INSERT INTO posts SET ?',newpost ,function(err,rows){
+                if (err) {
+                    res.send(err);
+                }else{
+                    res.send({status: "success"});
+                }
+                db.release();
+            });
+        }else{
+            res.send(err);
+        }
+    });
 };
 
-exports.deletePost = function(req, res) {
-    var id = req.params.id;
-    if (id >= 0 && id < data.posts.length) {
-        data.posts.splice(id, 1);
-        res.send({
-            status: "ok"
-        });
-    } else
-        res.send(404, {
-            status: "error"
-        });
-};
+// exports.editPost = function(req, res) {
+//     var id = req.params.id;
+//     if (id >= 0 && id < data.posts.length) {
+//         data.posts[id] = req.body;
+//         res.send({
+//             status: "ok"
+//         });
+//     } else
+//         res.send(404, {
+//             status: "error"
+//         });
+// };
+
+// exports.deletePost = function(req, res) {
+//     var id = req.params.id;
+//     if (id >= 0 && id < data.posts.length) {
+//         data.posts.splice(id, 1);
+//         res.send({
+//             status: "ok"
+//         });
+//     } else
+//         res.send(404, {
+//             status: "error"
+//         });
+// };
 
 
 
@@ -173,11 +239,10 @@ exports.deleteCategory = function(req, res) {
 };
 
 
-exports.db = function(req, res){
-    
+exports.users = function(req, res) {
     db.getConnection(function(err,db){
         if (!err) {
-            db.query('SELECT * FROM posts',function(err,rows){
+            db.query('SELECT * FROM users',function(err,rows){
                 if (err) {
                     res.send(err);
                 }else{
@@ -185,34 +250,26 @@ exports.db = function(req, res){
                 }
                 db.end();
             });
+        }else{
+            res.send(err);
         }
     });
 };
 
-
-exports.users = function(req, res) {
-    connection.connect();
-    connection.query('SELECT * FROM users', function(err, rows){
-        if (err) {
-            res.send(err);
+exports.user = function (req, res){
+    var login = req.param('login');
+    db.getConnection(function(err,db){
+        if (!err) {
+            db.query('SELECT * FROM users WHERE login=?',login ,function(err,rows){
+                if (err) {
+                    res.send(err);
+                }else{
+                    res.send({posts : rows});
+                }
+                db.end();
+            });
         }else{
-            res.send({posts : rows});
+            res.send(err);
         }
     });
-    connection.end();
-};
-
-exports.users = function(req, res) {
-    var login = req.params.login;
-
-
-    connection.connect();
-    connection.query('SELECT * FROM users WHERE login ', function(err, rows){
-        if (err) {
-            res.send(err);
-        }else{
-            res.send({posts : rows});
-        }
-    });
-    connection.end();
 };
