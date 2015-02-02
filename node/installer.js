90737ed3716d48ac2e91ff7dfe2085717dc7255c
gouxlord <goux.pierre@gmail.com>
2015-02-02 20:42:31 +0100

    installer now work only with arguments.

--- node/installer.js
+++ node/installer.js
@@ -1,3 +1,10 @@
 var NumeractiveInstaller = require('./installer/installer')
 
-NumeractiveInstaller.run();
+switch (process.argv[2]){
+    case "database":
+        NumeractiveInstaller.run();
+        break;
+    default:
+        console.log("You must pass an argument according to what you want to do:")
+        console.log("database           Configure database")
+}
