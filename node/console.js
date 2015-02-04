26a7d209f56cc905ebea8345a78d793da1109467
gouxlord <goux.pierre@gmail.com>
2015-02-04 22:49:35 +0100

    FIXE routes in commands

--- node/console.js
+++ node/console.js
@@ -8,7 +8,8 @@ switch (process.argv[2]){
         NumeractiveInstaller.addUser();
         break;
     default:
-        console.log("You must pass an argument according to what you want to do:")
-        console.log("database               Configure database")
-        console.log("add:admin              add:administrator")
+        console.log("You must pass an argument according to what you want to do:");
+        console.log("database               Configure database");
+        console.log("add:admin              add:administrator");
+        break;
 }
