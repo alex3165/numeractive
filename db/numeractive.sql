38e2f7bd24fd0fc3de44739a7d1ea6202356fb4c
Guillaume d'Eimar de Jabrun <guillaume.jab@gmail.com>
2015-02-17 16:48:37 +0100

    Little tweaks on the database shema

--- db/numeractive.sql
+++ db/numeractive.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.3.2
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Mar 06 Mai 2014 à 21:56
--- Version du serveur :  5.5.34
--- Version de PHP :  5.5.10
+-- Host: localhost
+-- Generation Time: Feb 17, 2015 at 04:45 PM
+-- Server version: 10.0.15-MariaDB
+-- PHP Version: 5.6.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,27 +17,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `numeractive`
+-- Database: `numeractive`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categories`
+-- Table structure for table `categories`
 --
 
-CREATE TABLE `categories` (
-  `id_cat` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(255) NOT NULL,
   `type` varchar(255) COLLATE utf8_bin NOT NULL,
-  `color` varchar(255) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id_cat`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
+  `color` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Contenu de la table `categories`
+-- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id_cat`, `type`, `color`) VALUES
+INSERT INTO `categories` (`id`, `type`, `color`) VALUES
 (0, 'programmation', 'blue'),
 (1, 'inspiration', 'yellow'),
 (2, 'creative coding', 'purple'),
@@ -47,65 +46,101 @@ INSERT INTO `categories` (`id_cat`, `type`, `color`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `posts`
+-- Table structure for table `posts`
 --
 
-CREATE TABLE `posts` (
-  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(255) unsigned NOT NULL,
   `title` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `text` mediumtext CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `img` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `creation` datetime NOT NULL,
+  `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_cat` int(255) NOT NULL,
-  `id_user` int(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_idcat` (`id_cat`),
-  KEY `id_user` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
+  `id_user` int(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Contenu de la table `posts`
+-- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`id`, `title`, `text`, `img`, `creation`, `id_cat`, `id_user`) VALUES
-(4, 'blablabla c''est le premier article', 'In Kazakhstan, it is illegal for more than five woman to be in the same place except for in brothel or in grave. In US and A, many womens meet in a groups called feminists.\n\nI arrived in America''s airport with clothings, US dollars, and a jar of gypsy tears to protect me from AIDS.', 'images/img7.png', '2014-04-23 18:30:59', 2, 1),
-(5, 'Qui frappuccino trifecta to go milk', 'Extraction skinny, sweet plunger pot wings turkish saucer single shot cappuccino, fair trade coffee as in, mazagran, caramelization cortado est acerbic cortado roast sit siphon. Siphon fair trade, est filter est siphon extraction instant crema, cup, shop, aged sugar to go filter variety at roast seasonal barista seasonal. Macchiato, half and half iced medium chicory single shot, whipped, galão, strong aromatic, a bar milk americano plunger pot. Carajillo half and half sweet et cappuccino est chicory, steamed aftertaste instant in spoon rich spoon galão frappuccino, organic, café au lait in shop dripper eu steamed. Blue mountain instant, as roast extraction arabica brewed cup, body iced, pumpkin spice to go id organic frappuccino caffeine percolator cup qui percolator cream.', 'images/img2.png', '2014-04-29 18:19:09', 3, 1),
-(6, 'Skinny, coffee aromatic, acerbic crema eu acerbic single shot', 'Aromatic, sit french press cinnamon, aftertaste java aroma and, dripper variety qui to go, at to go affogato macchiato extra carajillo saucer affogato con panna. Grounds, frappuccino cinnamon turkish espresso filter seasonal, so whipped, spoon to go that aromatic flavour. Spoon trifecta latte, flavour macchiato brewed strong so whipped spoon acerbic mocha rich. Milk, cream seasonal decaffeinated fair trade et robust to go grounds, extra galão, mocha latte cup, aged shop beans, cortado crema macchiato mocha steamed.', 'images/img1.jpg', '2014-04-29 18:20:09', 2, 3),
-(7, 'Leave dead animals as gifts climb leg sun bathe', 'Hate dog climb leg or chase imaginary bugs. Nap all day. Swat at dog sleep on keyboard yet leave dead animals as gifts so hunt anything that moves leave dead animals as gifts or intrigued by the shower chase imaginary bugs. Hate dog destroy couch and need to chase tail. Stick butt in face destroy couch for leave hair everywhere and shake treat bag chew iPad power cord for throwup on your pillow for missing until dinner time. Chase imaginary bugs.', 'images/img5.jpg', '2014-04-29 18:20:57', 4, 1);
+(4, 'blablabla c''est le premier article', 'In Kazakhstan, it is illegal for more than five woman to be in the same place except for in brothel or in grave. In US and A, many womens meet in a groups called feminists.\n\nI arrived in America''s airport with clothings, US dollars, and a jar of gypsy tears to protect me from AIDS.', 'images/img7.png', '2014-04-23 18:30:59', 2, 4),
+(5, 'Qui frappuccino trifecta to go milk', 'Extraction skinny, sweet plunger pot wings turkish saucer single shot cappuccino, fair trade coffee as in, mazagran, caramelization cortado est acerbic cortado roast sit siphon. Siphon fair trade, est filter est siphon extraction instant crema, cup, shop, aged sugar to go filter variety at roast seasonal barista seasonal. Macchiato, half and half iced medium chicory single shot, whipped, galão, strong aromatic, a bar milk americano plunger pot. Carajillo half and half sweet et cappuccino est chicory, steamed aftertaste instant in spoon rich spoon galão frappuccino, organic, café au lait in shop dripper eu steamed. Blue mountain instant, as roast extraction arabica brewed cup, body iced, pumpkin spice to go id organic frappuccino caffeine percolator cup qui percolator cream.', 'images/img2.png', '2014-04-29 18:19:09', 3, 4),
+(6, 'Skinny, coffee aromatic, acerbic crema eu acerbic single shot', 'Aromatic, sit french press cinnamon, aftertaste java aroma and, dripper variety qui to go, at to go affogato macchiato extra carajillo saucer affogato con panna. Grounds, frappuccino cinnamon turkish espresso filter seasonal, so whipped, spoon to go that aromatic flavour. Spoon trifecta latte, flavour macchiato brewed strong so whipped spoon acerbic mocha rich. Milk, cream seasonal decaffeinated fair trade et robust to go grounds, extra galão, mocha latte cup, aged shop beans, cortado crema macchiato mocha steamed.', 'images/img1.jpg', '2014-04-29 18:20:09', 2, 4),
+(7, 'Leave dead animals as gifts climb leg sun bathe', 'Hate dog climb leg or chase imaginary bugs. Nap all day. Swat at dog sleep on keyboard yet leave dead animals as gifts so hunt anything that moves leave dead animals as gifts or intrigued by the shower chase imaginary bugs. Hate dog destroy couch and need to chase tail. Stick butt in face destroy couch for leave hair everywhere and shake treat bag chew iPad power cord for throwup on your pillow for missing until dinner time. Chase imaginary bugs.', 'images/img5.jpg', '2014-04-29 18:20:57', 4, 4);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(255) NOT NULL,
+  `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `name` tinytext COLLATE utf8_bin NOT NULL,
   `login` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `mdp` char(128) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+  `mdp` char(128) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `creation`, `name`, `login`, `mdp`) VALUES
-(1, '2014-04-28 12:58:08', 0x6d617263656c696e6f, 'admin', 'porcel'),
-(3, '2014-04-28 12:58:08', 0x6d617263656c696e6f, 'marcel', 'porcel');
+(4, '2015-02-17 16:38:28', 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
 
 --
--- Contraintes pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `posts`
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `type` (`type`);
+
+--
+-- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `fk_idcat` FOREIGN KEY (`id_cat`) REFERENCES `categories` (`id_cat`),
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`id`), ADD KEY `fk_idcat` (`id_cat`), ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(255) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+ADD CONSTRAINT `fk_idcat` FOREIGN KEY (`id_cat`) REFERENCES `categories` (`id`),
+ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
